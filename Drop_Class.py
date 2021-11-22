d7e75558c3a65c08a66867da337a0062e85e96ab
joeressler <joe.c.ressler@gmail.com>
2021-11-22 09:37:39 -0500

    Update Drop_Class.py

--- Drop_Class.py
+++ Drop_Class.py
@@ -7,5 +7,5 @@ class Drop:
         self.p4 = p4
     def __repr__(self):
         
-        s1 = self.name + "\nP(1):" + str(self.p1 * 100) + "% \nP(2):" + str(self.p2 * 100) + "% \nP(3):" + str(self.p3 * 100) + "% \nP(4):" + str(self.p4 * 100) + "%"
+        s1 = self.name + "\nP(1):" + str(int(self.p1 * 100)) + "% \nP(2):" + str(int(self.p2 * 100)) + "% \nP(3):" + str(int(self.p3 * 100)) + "% \nP(4):" + str(int(self.p4 * 100)) + "%"
         return s1
