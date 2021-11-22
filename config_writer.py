7bb5fa9a9145d57ae6461d8fe6f79559a887de4e
joeressler <joe.c.ressler@gmail.com>
2021-11-22 10:01:40 -0500

    Update config_writer.py

--- config_writer.py
+++ config_writer.py
@@ -12,3 +12,4 @@ json_object = json.dumps(config, indent = 4)
 name = "config_" + str(uuid.uuid4()) + ".json"
 with open(name, "w") as outfile:
     outfile.write(json_object)
+print(name + " successfully created.")
