8dd690b23d223f7462db8b4b409ef32050183702
joeressler <joe.c.ressler@gmail.com>
2021-11-22 09:56:07 -0500

    creating config_writer

---
+++ config_writer.py
@@ -0,0 +1,14 @@
+import json
+import uuid
+config = {
+    "name" : "Potions",
+    "p1" : "0.1",
+    "p2" : "0.5",
+    "p3" : "0.2",
+    "p4" : "0.2"
+}
+
+json_object = json.dumps(config, indent = 4)
+name = "config_" + str(uuid.uuid4()) + ".json"
+with open(name, "w") as outfile:
+    outfile.write(json_object)
